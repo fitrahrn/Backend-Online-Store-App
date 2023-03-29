afdd165ff8d5aad738e4cf6e29729ebdb73a1909
Fitrah Ramadhani Nugroho <fitnug30@gmail.com>
2023-03-30 00:33:20 +0700

    fix connecting database

--- initializers/database.go
+++ initializers/database.go
@@ -14,11 +14,11 @@ var DB *gorm.DB
 func ConnectToDB() {
 	var err error
 	host := os.Getenv("HOST")
-	// user := os.Getenv("USER")
-	// pass := os.Getenv("PASSWORD")
-	// port := os.Getenv("PORT")
-	// name := os.Getenv("NAME")
-	connStr := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Asia/Jakarta", host, "postgres", "postgres", "online_stores", 5432)
+	user := os.Getenv("USER")
+	pass := os.Getenv("PASS")
+	//port := os.Getenv("PORT")
+	name := os.Getenv("NAME")
+	connStr := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Asia/Jakarta", host, user, pass, name, 5432)
 	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
